fad689ec9159ea8e093705379fc80b4bee8ad4f8
Meet <meet57.mp@gmail.com>
2021-09-23 22:56:30 +0530

    para

--- assests/script.js
+++ assests/script.js
@@ -14,3 +14,15 @@ closeNavDelay = () => {
         MobileNav.classList.add("swipeout")
     }, 100);
 }
+
+showDes = (id) => {
+    para = id+'des'
+    console.log(para)
+    document.getElementById(para).classList.remove('visually-hidden')
+}
+
+hideDes = (id) => {
+    para = id+'des'
+    console.log(para)
+    document.getElementById(para).classList.add('visually-hidden')
+}
